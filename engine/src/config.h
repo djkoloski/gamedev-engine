680682c9e177aa9ddf7e0ba81bfbb5f8112ae337
David Koloski <djkoloski@gmail.com>
2016-02-20 11:53:30 -0500

    Added GLEW, updated CMake for Win32/64 and Cygwin, added code to render a triangle

--- engine/src/config.h
+++ engine/src/config.h
@@ -1,2 +1,6 @@
 #define GE_VERSION_MAJOR 0
 #define GE_VERSION_MINOR 0
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,17 +1,53 @@
 #include <iostream>
 #include "config.h"
+#include <glew.h>
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
 
 int main(int argc, char **argv)
 {
 	std::cout << "GE version: " << GE_VERSION_MAJOR << "." << GE_VERSION_MINOR << std::endl;
-	std::cout << "Hello world!" << std::endl;
 
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-		return 1;
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+
+	SDL_Window *window = SDL_CreateWindow("GE", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+
+	SDL_GLContext context;
+	context = SDL_GL_CreateContext(window);
+	SDL_GL_MakeCurrent(window, context);
+
+	glViewport(0, 0, 1280, 720);
+
+	SDL_Event e;
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	bool shouldClose = false;
+	while (!shouldClose)
+	{
+		while (SDL_PollEvent(&e))
+		{
+			if (e.type == SDL_QUIT)
+				shouldClose = true;
+		}
+
+		glBegin(GL_TRIANGLES);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glColor3f(1.0f, 1.0f, 1.0f);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glEnd();
+
+		SDL_GL_SwapWindow(window);
 	}
+
+	SDL_GL_DeleteContext(context);
+	SDL_DestroyWindow(window);
 	SDL_Quit();
+
 	return 0;
 }
