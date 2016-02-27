f27b6cf2c9d737bc4bd65da4fa1976019ae98124
Andrew Dunetz <duneta@rpi.edu>
2016-02-27 16:43:38 -0500

    add containers folders and array files to the project directories.

---
+++ engine/include/containers/array.h
@@ -0,0 +1,13 @@
+#ifndef GE_ARRAY_H_
+#define GE_ARRAY_H_
+
+namespace GE 
+{
+	namespace Containers
+	{
+		
+		
+	}
+}
+
+#endif /* GE_ARRAY_H_ */
---
+++ engine/src/containers/array.cpp
@@ -0,0 +1,15 @@
+#ifndeg GE_ARRAY_CPP_
+#define GE_ARRAY_CPP_
+
+#include <containers/array.h>
+
+namespace GE 
+{
+	namespace Containers
+	{
+		
+		
+	}
+}
+
+#endif /* GE_ARRAY_CPP_ */
